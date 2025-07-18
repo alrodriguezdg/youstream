07142a87778d9727d174fd6eaf9f0f9f17157ee9
alrodriguezdg <alrodriguezdg@gmail.com>
2025-07-18 16:25:49 -0500

    Backend update

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ import os
 YOUTUBE_API_KEY = 'AIzaSyD0abX4a4P19KNuNaWHP2x9i-7hNuF3OIg'
 
 # Database Configuration
-DATABASE_URI = 'sqlite:///youstream.db'
+DATABASE_URI = 'sqlite:///youstream.db' # Cambiar a la ruta de la base de datos
 
 # Flask Configuration
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-change-this-in-production')
